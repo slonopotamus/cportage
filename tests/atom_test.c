c808d6783a56513d5b8306ec6292b370c4a6a603
Marat Radchenko <marat@slonopotamus.org>
2009-10-15 09:18:10 +0000

    added atom tests

---
+++ tests/atom_test.c
@@ -0,0 +1,134 @@
+/*
+	Copyright 2009, Marat Radchenko
+
+	This file is part of cportage.
+
+	cportage is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	cportage is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with cportage.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdbool.h>
+#include <stdio.h>
+
+#include "cportage/atom.h"
+
+struct item {
+	const char * str;
+	bool valid;
+};
+
+int main() {
+	struct item data[] = {
+		{ "sys-apps/portage", true },
+		{ "=sys-apps/portage-2.1", true },
+		{ "=sys-apps/portage-2.1*", true },
+		{ ">=sys-apps/portage-2.1", true },
+		{ "<=sys-apps/portage-2.1", true },
+		{ ">sys-apps/portage-2.1", true },
+		{ "<sys-apps/portage-2.1", true },
+		{ "~sys-apps/portage-2.1", true },
+		{ "sys-apps/portage:foo", true },
+		{ "sys-apps/portage-2.1:foo", false },
+		{ "sys-apps/portage-2.1:", false },
+		{ ">~cate-gory/foo-1.0", false },
+		{ ">~category/foo-1.0", false },
+		{ "<~category/foo-1.0", false },
+		{ "###cat/foo-1.0", false },
+		{ "~sys-apps/portage", false },
+		{ "portage", false },
+		{ "=portage", false },
+		{ ">=portage-2.1", false },
+		{ "~portage-2.1", false },
+		{ "=portage-2.1*", false },
+		{ "null/portage", true },
+		{ "null/portage*:0", false },
+		{ ">=null/portage-2.1", true },
+		{ ">=null/portage", false },
+		{ ">null/portage", false },
+		{ "=null/portage*", false },
+		{ "=null/portage", false },
+		{ "~null/portage", false },
+		{ "<=null/portage", false },
+		{ "<null/portage", false },
+		{ "~null/portage-2.1", true },
+		{ "=null/portage-2.1*", true },
+		{ "null/portage-2.1*", false },
+		{ "app-doc/php-docs-20071125", false },
+		{ "app-doc/php-docs-20071125-r2", false },
+		{ "=foo/bar-1-r1-1-r1", false },
+		{ "foo/-z-1", false },
+
+		/*
+			These are invalid because pkg name must not end in hyphen
+			followed by version number
+		 */
+		{ "=foo/bar-1-r1-1-r1", false },
+		{ "=foo/bar-123-1", false },
+		{ "=foo/bar-123-1*", false },
+		{ "foo/bar-123", false },
+		{ "=foo/bar-123-1-r1", false },
+		{ "=foo/bar-123-1-r1*", false },
+		{ "foo/bar-123-r1", false },
+		{ "foo/bar-1", false },
+
+		{ "=foo/bar--baz-1-r1", true },
+		{ "=foo/bar-baz--1-r1", true },
+		{ "=foo/bar-baz---1-r1", true },
+		{ "=foo/bar-baz---1", true },
+		{ "=foo/bar-baz-1--r1", false },
+		{ "games-strategy/ufo2000", true },
+		{ "~games-strategy/ufo2000-0.1", true },
+		{ "=media-libs/x264-20060810", true },
+		{ "foo/b", true },
+		{ "app-text/7plus", true },
+		{ "foo/666", true },
+		{ "=dev-libs/poppler-qt3-0.11*", true },
+
+		#warning TODO: Uncomment this when useflags parsing is written.
+		/*
+		
+			  ( "sys-apps/portage-2.1:[foo]", False ),
+			  ( "=sys-apps/portage-2.2*:foo[bar?,!baz?,!doc=,build=]", True ),
+			  ( "=sys-apps/portage-2.2*:foo[doc?]", True ),
+			  ( "=sys-apps/portage-2.2*:foo[!doc?]", True ),
+			  ( "=sys-apps/portage-2.2*:foo[doc=]", True ),
+			  ( "=sys-apps/portage-2.2*:foo[!doc=]", True ),
+			  ( "=sys-apps/portage-2.2*:foo[!doc]", False ),
+			  ( "=sys-apps/portage-2.2*:foo[!-doc]", False ),
+			  ( "=sys-apps/portage-2.2*:foo[!-doc=]", False ),
+			  ( "=sys-apps/portage-2.2*:foo[!-doc?]", False ),
+			  ( "=sys-apps/portage-2.2*:foo[-doc?]", False ),
+			  ( "=sys-apps/portage-2.2*:foo[-doc=]", False ),
+			  ( "=sys-apps/portage-2.2*:foo[-doc!=]", False ),
+			  ( "=sys-apps/portage-2.2*:foo[-doc=]", False ),
+			  ( "=sys-apps/portage-2.2*:foo[bar][-baz][doc?][!build?]", False ),
+			  ( "=sys-apps/portage-2.2*:foo[bar,-baz,doc?,!build?]", True ),
+			  ( "=sys-apps/portage-2.2*:foo[bar,-baz,doc?,!build?,]", False ),
+			  ( "=sys-apps/portage-2.2*:foo[,bar,-baz,doc?,!build?]", False ),
+			  ( "=sys-apps/portage-2.2*:foo[bar,-baz][doc?,!build?]", False ),
+			  ( "=sys-apps/portage-2.2*:foo[bar][doc,build]", False ),
+		*/
+	};
+	int retval = 0;
+	for (unsigned int i = 0; i < sizeof(data) / sizeof(data[0]); ++i) {
+		const char * s = data[i].str;
+		const char * msg = data[i].valid ? "valid" : "invalid";
+		void * atom = new(Class(Atom), s);
+		if (data[i].valid == (atom == NULL)) {
+			fprintf(stderr, "'%s' must be %s, but it isn't", s, msg);
+			--retval;
+		}
+		unref(atom);
+	}
+	return retval;
+}
