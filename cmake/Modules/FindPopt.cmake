5dba7c9e66f1e69fa0721063aa52e7eac1733905
Marat Radchenko <marat@slonopotamus.org>
2010-01-20 18:00:39 +0300

    use lowercase in cmake files

--- cmake/Modules/FindPopt.cmake
+++ cmake/Modules/FindPopt.cmake
@@ -5,25 +5,24 @@
 #  Popt_LIBRARIES   - List of libraries when using popt.
 #  Popt_FOUND       - True if popt found.
 
-
-IF (Popt_INCLUDE_DIR)
+if(Popt_INCLUDE_DIR)
   # Already in cache, be silent
-  SET(Popt_FIND_QUIETLY TRUE)
-ENDIF (Popt_INCLUDE_DIR)
+  set(Popt_FIND_QUIETLY TRUE)
+endif()
 
-FIND_PATH(Popt_INCLUDE_DIR popt.h)
+find_path(Popt_INCLUDE_DIR popt.h)
 
-FIND_LIBRARY(Popt_LIBRARY NAMES popt )
+find_library(Popt_LIBRARY NAMES popt)
 
-# handle the QUIETLY and REQUIRED arguments and set Popt_FOUND to TRUE if 
+# handle the QUIETLY and REQUIRED arguments and set Popt_FOUND to TRUE if
 # all listed variables are TRUE
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Popt DEFAULT_MSG Popt_LIBRARY Popt_INCLUDE_DIR)
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Popt DEFAULT_MSG Popt_LIBRARY Popt_INCLUDE_DIR)
 
-IF(Popt_FOUND)
-  SET( Popt_LIBRARIES ${Popt_LIBRARY} )
-ELSE(Popt_FOUND)
-  SET( Popt_LIBRARIES )
-ENDIF(Popt_FOUND)
+if(Popt_FOUND)
+  set(Popt_LIBRARIES ${Popt_LIBRARY})
+else()
+  set(Popt_LIBRARIES)
+endif()
 
-MARK_AS_ADVANCED( Popt_LIBRARY Popt_INCLUDE_DIR )
+mark_as_advanced(Popt_LIBRARY Popt_INCLUDE_DIR)
