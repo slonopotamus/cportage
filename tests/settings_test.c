b4650f9524715943c7f15eb5f0405aa3d0f03c0d
Marat Radchenko <marat@slonopotamus.org>
2011-04-05 20:11:39 +0400

    added test for incrementals support, doesn't pass yet

--- tests/settings_test.c
+++ tests/settings_test.c
@@ -34,6 +34,21 @@ profiles_order(void) {
     g_free(root);
 }
 
+static void
+incrementals(void) {
+    char *root = g_build_filename(dir, "roots/incrementals", NULL);
+    GError *error = NULL;
+    CPSettings settings = cp_settings_new(root, &error);
+    const char *use;
+
+    g_assert_no_error(error);
+    use = cp_settings_get(settings, "USE");
+    g_assert_cmpstr(use, ==, "use1 -use2 use3 -use4 use5 -use6");
+
+    cp_settings_unref(settings);
+    g_free(root);
+}
+
 int
 main(int argc, char *argv[]) {
     g_test_init(&argc, &argv, NULL);
@@ -41,7 +56,8 @@ main(int argc, char *argv[]) {
     g_assert(argc == 2);
     dir = argv[1];
 
-    g_test_add_func("/settings/profiles/order", profiles_order);
+    g_test_add_func("/settings/profile_order", profiles_order);
+    g_test_add_func("/settings/incrementals", incrementals);
 
     return g_test_run();
 }
